53037247000e341c3d4ecc8b642b50115139f3ac
Posivnich <lposivnich@code-pilots.ru>
2019-10-06 11:58:56 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+      <emoji-area></emoji-area>
+  </div>
+</template>
+
+<script>
+import EmojiArea from './components/EmojiArea'
+
+export default {
+  name: 'app',
+  components: {
+    EmojiArea
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    css: {
+        loaderOptions: {
+            sass: {
+                data: `@import "./src/assets/scss/base/_variables.scss";`
+            },
+        }
+    },
+};
